c2034796da40981a2e7fc43b5c8c5de4beb63344
Imtiyaz0000 <159421205+Imtiyaz0000@users.noreply.github.com>
2024-05-24 22:16:32 +0100

    removed unnecessary comments

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
-/*function matrixToString(matrix: number[][]): string {
-  let result = '';
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      result += matrix[i][j].toString() + '\t'; // Separate each element by a tab
-    }
-    result += '\n'; // Move to the next line after each row
-  }
-  return result;
-}*/
-
 async function inverter() {
   let container = document.querySelector(".container");
   let matrix: string[][] = []
@@ -38,12 +27,6 @@ async function inverter() {
 [${result[2][0]}][${result[2][1]}][${result[2][2]}]
 `;
   }
-  /*if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }*/
 }
 
 window.addEventListener("DOMContentLoaded", () => {
